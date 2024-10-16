aeb391bed20c3408b2b4e67ff4d6f4c73d951681
Drenyer <franconilvermamanimamani@gmail.com>
2024-10-16 18:27:27 -0500

    Initial Commit

---
+++ MMamani2Snapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MMamani2Snapchat
+//
+//  Created by Drenyer on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
